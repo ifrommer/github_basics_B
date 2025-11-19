f86f6e4bf5b4ed09eb9a2c57032dcaca04291094
Frommer <Ian.Frommer@uscga.edu>
2025-11-19 12:50:19 -0500

    hummus!

--- recipes.py
+++ recipes.py
@@ -1,6 +1,7 @@
 recipes = {
     "apple pie": ["apples", "flour", "sugar"],
-    "pancakes": ["flour", "milk", "eggs"]
+    "pancakes": ["flour", "milk", "eggs"],
+    "hummus": ["chick peas","garlic", "olive oil"]
 }
 # the other Dr. Frommer was here
 # Prof. Frommer was here
